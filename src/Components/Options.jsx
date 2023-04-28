3f5138d0db175108ef9647b5f8fc2bd4497087f4
Vinicius rodrigues <vinicius.motta.09@hotmail.com>
2023-04-27 23:36:53 -0300

    1.3

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -4,17 +4,17 @@ import "./Options.css";
 const FloatingButtonOptions = () => {
   return (
     <ul className="floating-button-options">
-      <a href="#">
+      <a href="https://clientes.consorcionacionalhonda.com.br/Seguranca/Login?_ga=2.266393545.693823917.1682553218-756674096.1679608111">
         <li>
           Próximas assembléias <i class="fa-solid fa-calendar-days"></i>
         </li>
       </a>
-      <a href="#">
+      <a href="https://wa.me/5186140160" target="_blank">
         <li>
           Whats App <i class="fa-brands fa-whatsapp"></i>
         </li>
       </a>
-      <a href="#">
+      <a href="tel:+08000071024" target="_blank">
         <li>
           Entre em contato pelo 0800 <i class="fa-solid fa-phone"></i>
         </li>
