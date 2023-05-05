8be5bc8d21ef0172b4189f6c0df6770e8d37c101
Vinicius rodrigues <vinicius.motta.09@hotmail.com>
2023-05-05 10:27:09 -0300

    Updating  analytics 4

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { StateValue } from "./UserContext";
 import ReactGA from "react-ga";
 
 // const TRACKING_ID = "UA-267255567-1";
-const TRACKING_ID = "921489480582";
+const TRACKING_ID = "G-FBJ3LE1H0N";
 ReactGA.initialize(TRACKING_ID);
 
 function App() {
